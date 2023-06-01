39f137c4e35ec03d504e1165d00ad4dc5d875641
Ilya Tzoop <tolya12345w@gmail.com>
2023-06-01 02:34:53 +0200

    improve bwt.py

--- bwt.py
+++ bwt.py
@@ -2,48 +2,66 @@ import sys
 import os
 import json
 
+# Start of text marker and end of text marker
+SOTM = "~"
+EOTM = "#"
 
-def BWT_encode(s):
+
+def text_markers_present(s):
+    return SOTM in s or EOTM in s
+
+
+def bwt_encode(s):
     """Apply Burrows-Wheeler transform to input string."""
-    assert "\002" not in s and "\003" not in s, "Input string cannot contain STX and ETX characters"
-    s = "\002" + s + "\003"  # Add start and end of text marker
+    if text_markers_present(s):
+        print(f"Input string cannot contain SOTM ('{SOTM}') and EOTM ('{EOTM}') characters")
+        sys.exit(1)
+    s = SOTM + s + EOTM  # Add start and end of text marker
     table = sorted(s[i:] + s[:i] for i in range(len(s)))  # Table of rotations of string
     last_column = [row[-1:] for row in table]  # Last characters of each row
     return "".join(last_column)  # Convert list of characters into string
 
 
-def BWT_decode(s):
-    """Decode Burrows-Wheeler transform of input string."""
-    table = [""] * len(s)  # Initialize table
-    for _ in s:
-        table = sorted(s[i] + table[i] for i in range(len(s)))  # Insert characters of s
-    # Find row that ends with ETX character
-    for row in table:
-        if row.endswith("\003"):
-            return row.rstrip("\003").lstrip("\002")  # Strip STX and ETX characters
-
-
 def main(input_file, output_file):
     # Check if input file exists
     if not os.path.isfile(input_file):
-        print(f"Error: Input file '{input_file}' does not exist.")
+        print(f"Error: Input file '{input_file}' does not exist")
         sys.exit(1)
 
     # Check if output path is valid
     if not os.path.isdir(os.path.dirname(output_file)):
-        print(f"Error: Output path '{os.path.dirname(output_file)}' does not exist.")
+        print(f"Error: Output path '{os.path.dirname(output_file)}' does not exist")
         sys.exit(1)
 
     with open(input_file, 'r') as f:
-        data = json.load(f)
+        try:
+            data = json.load(f)
+        except json.JSONDecodeError:
+            print(f"Error: Input file '{input_file}' is not a valid JSON file")
+            sys.exit(1)
 
-    # encode 'instruction' and 'input' fields
+    # Check if data is a list and each item is a dictionary with correct keys
+    if not isinstance(data, list):
+        print(f"Error: Input file '{input_file}' does not contain a list at the top level")
+        sys.exit(1)
+
+    skipped = 0
     for item in data:
-        item['instruction'] = BWT_encode(item['instruction'])
-        item['input'] = BWT_encode(item['input'])
+        if not isinstance(item, dict) or 'instruction' not in item or 'input' not in item or 'output' not in item:
+            print(f"Error: Input file '{input_file}' has incorrect structure")
+            sys.exit(1)
+        instr = item['instruction']
+        inp = item['input']
+        if text_markers_present(instr) or text_markers_present(inp):
+            skipped += 1
+            continue
+        item['instruction'] = bwt_encode(instr)
+        item['input'] = bwt_encode(inp)
 
     with open(output_file, 'w') as f:
-        json.dump(data, f)
+        json.dump(data, f, indent=4)
+
+    print(f"Written file to '{output_file}', {skipped} entries were skipped due to SOTM or EOTM presence")
 
 
 if __name__ == "__main__":
