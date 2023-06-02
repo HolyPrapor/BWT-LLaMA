cda454584381fc281cc686e8b3a9196aeaf1c16d
LexLippi <43090493+LexLippi@users.noreply.github.com>
2023-06-02 22:43:35 +0500

    Create lz_77.py

---
+++ lz_77.py
@@ -0,0 +1,35 @@
+def find_matching(search, lookahead):
+    best_length = 0
+    best_position = -1
+
+    for i in range(len(search)):
+        length = 0
+
+        while (length < len(lookahead) and length + i < len(search) and search[i + length] == lookahead[length]):
+            length += 1
+
+        if (best_length < length):
+            best_length = length
+            best_position = len(search) - i
+
+    return best_position, best_length
+
+def lz77_encode(input_string, search_len=20):
+    ans = []
+    pos = 0
+    search = ''
+    lookahead = input_string
+
+    while pos < len(input_string):
+        offset, length = find_matching(search, lookahead)
+        if offset == -1:
+            ans.append(input_string[pos])
+            pos += 1
+        else:
+            ans.append("<{},{}>".format(offset, length))
+            pos += length
+
+        search = input_string[max(0, pos - search_len):pos]
+        lookahead = input_string[pos:]
+
+    return "".join(ans)
