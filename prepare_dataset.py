ac1610fc34b6b45334a711eefa98aae8ce4840d2
Ilya Tzoop <tolya12345w@gmail.com>
2023-06-11 14:08:23 +0200

    fix small issues in prepare_dataset.py

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -6,7 +6,7 @@ from bwt import bwt_encode, text_markers_present
 from lz_77 import lz77_encode
 
 
-def prepare(input_file, output_file, encode, filter=None):
+def prepare(input_file, output_file, encode, filter=None, limit=100000):
     # Check if input file exists
     if not os.path.isfile(input_file):
         print(f"Error: Input file '{input_file}' does not exist")
@@ -29,7 +29,9 @@ def prepare(input_file, output_file, encode, filter=None):
         print(f"Error: Input file '{input_file}' does not contain a list at the top level")
         sys.exit(1)
 
-    skipped = 0
+    skipped = max(len(data) - limit, 0)
+    data = data[-limit:]
+    new_data = []
     for item in data:
         if not isinstance(item, dict) or 'instruction' not in item or 'input' not in item or 'output' not in item:
             print(f"Error: Input file '{input_file}' has incorrect structure")
@@ -39,11 +41,19 @@ def prepare(input_file, output_file, encode, filter=None):
         if filter and (filter(instr) or filter(inp)):
             skipped += 1
             continue
-        item['instruction'] = encode(instr)
-        item['input'] = encode(inp)
+        instr = encode(instr)
+        inp = encode(inp)
+        if len(instr) > 510 or len(inp) > 510 or len(item['output']) > 510:
+            skipped += 1
+            continue
+        new_data.append({
+            'instruction': instr,
+            'input': inp,
+            'output': item['output']
+        })
 
     with open(output_file, 'w') as f:
-        json.dump(data, f, indent=4)
+        json.dump(new_data, f, indent=4)
 
     print(f"Written file to '{output_file}', {skipped} entries were skipped")
 
@@ -61,9 +71,10 @@ if __name__ == "__main__":
     parser.add_argument('input_file', type=str, help='Path to the input file')
     parser.add_argument('output_file', type=str, help='Path to the output file')
     parser.add_argument('--method', type=str, default='BWT', choices=['BWT', 'LZ77'], help='Encoding method to use')
+    parser.add_argument('--limit', type=int, default=100000, help='Choose number of entries to pick from the dataset')
 
     args = parser.parse_args()
 
     encode, encode_filter = choose_encode_and_filter(args.method)
 
-    prepare(args.input_file, args.output_file, encode, encode_filter)
+    prepare(args.input_file, args.output_file, encode, encode_filter, args.limit)
